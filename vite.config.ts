c3bb8868a38ccf08483d056117b072c3fa94a290
DelphyM <dmladek@gmail.com>
2024-12-25 15:09:43 +1300

    Setup GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+const base =
+  process.env.NODE_ENV === 'production'
+    ? '/delphym.github.io/'
+    : '/~delphym/REACT/delphym.github.io/'
+
 export default defineConfig({
+  base,
   plugins: [
     react(),
     viteStaticCopy({
